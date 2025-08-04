08c94f86675b5dcae66c2a41722d333ad8265582
Nadya Yablonovskaya <yablonovskayanadezhda@yandex.ru>
2025-08-04 20:47:08 +0100

    Исправлены тесты

--- test_books_collector.py
+++ test_books_collector.py
@@ -1,13 +1,11 @@
 import pytest
 from main import BooksCollector
-@pytest.fixture
-def books_collector():
-    return BooksCollector
 
 
 class TestBooksCollector:
 
-    def test_add_new_book_one_amount_book_added(books_collector):
+    def test_add_new_book_one_amount_book_added(self):
+        books_collector = BooksCollector()
         book_name = 'Преступление и наказание'
         assert book_name not in books_collector.get_books_genre()
         books_collector.add_new_book(book_name)
@@ -15,14 +13,16 @@ class TestBooksCollector:
 #тест на успешное добавление книги в словарь
 
 
-    def test_set_book_genre_one_book_book_added(books_collector):
+    def test_set_book_genre_one_book_book_added(self):
+        books_collector = BooksCollector()
         books_collector.add_new_book('Внутри убийцы')
         books_collector.set_book_genre('Внутри убийцы', 'Детективы')
         assert books_collector.get_books_genre()['Внутри убийцы'] == 'Детективы'
 #тест на успешное присвоение жанра новой книги
 
 
-    def test_get_book_genre_with_valid_genre(books_collector):
+    def test_get_book_genre_with_valid_genre(self):
+        books_collector = BooksCollector()
         books_collector.add_new_book('Книга_Фантастика')
         books_collector.set_book_genre('Книга_Фантастика', 'Фантастика')
         #добавили одну книгу
@@ -42,14 +42,16 @@ class TestBooksCollector:
 #тест на вывод книг с определенным жанром
 
 
-    def test_get_book_genre_for_existing_book(books_collector):
+    def test_get_book_genre_for_existing_book(self):
+        books_collector = BooksCollector()
         books_collector.add_new_book('Идеальный муж')
         books_collector.set_book_genre('Идеальный муж', 'Комедии')
         assert books_collector.get_book_genre('Идеальный муж') == 'Комедии'
 #тест на вывод жанра книги по ее имени
 
 
-    def test_get_books_genre_returns_dictionary(books_collector):
+    def test_get_books_genre_returns_dictionary(self):
+        books_collector = BooksCollector()
         books_collector.add_new_book('Кладбище домашних животных')
         books_collector.set_book_genre('Кладбище домашних животных', 'Ужасы')
         books_dictionary = books_collector.get_books_genre()
@@ -59,7 +61,8 @@ class TestBooksCollector:
 #тест на вывод текущего словаря
 
 
-    def test_get_books_for_children_only(books_collector):
+    def test_get_books_for_children_only(self):
+        books_collector = BooksCollector()
         books_collector.add_new_book('Король лев')
         books_collector.set_book_genre('Король лев', 'Мультфильмы')
         result = list(books_collector.get_books_for_children())
@@ -67,7 +70,8 @@ class TestBooksCollector:
 #тест на возвращение детских книг
 
 
-    def test_add_book_in_favorites_book_added(books_collector):
+    def test_add_book_in_favorites_book_added(self):
+        books_collector = BooksCollector()
         books_collector.add_new_book('Какая-то книга')
         books_collector.add_book_in_favorites('Какая-то книга')
         favorites_books = list(books_collector.get_list_of_favorites_books())
@@ -75,17 +79,19 @@ class TestBooksCollector:
 #тест на добавление книги в избранное
 
 
-    def test_get_list_of_favorites_books_empty(books_collector):
+    def test_get_list_of_favorites_books_empty(self):
+        books_collector = BooksCollector()
         books_collector.add_new_book('Книга 1')
         books_collector.add_new_book('Книга 2')
         books_collector.add_new_book('Книга 3')
 
         books_collector.add_book_in_favorites('Книга 1')
-        books_collector.add_book_in_vaforites('Книга 3')
+        books_collector.add_book_in_favorites('Книга 3')
 
         favorites = list(books_collector.get_list_of_favorites_books())
 
-        assert 'Книга 1' and 'Книга 3' in favorites
+        assert 'Книга 1' in favorites
+        assert 'Книга 3' in favorites
         assert 'Книга 2' not in favorites
         assert len(favorites) == 2
 #тест на вывод списка избранных книг
